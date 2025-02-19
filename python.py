3b7b1858363d433b31e752e396ea112ff0dd0f64
adamekmarcell <adamek.marcelll@gmail.com>
2025-02-19 14:22:48 +0100

    Első feladat elkészült

---
+++ python.py
@@ -0,0 +1,59 @@
+class Orszag:
+    def __init__(self, nev = "-", fovaros = "-", terulet = 0, nepesseg = 0, nepsuruseg = 0.0):
+        self.nev = nev
+        self.fovaros = fovaros
+        self.terulet = terulet
+        self.nepesseg = nepesseg
+        self.nepsuruseg = nepsuruseg
+
+    def info(self):
+        return f"Ország: {self.nev}, népesség: {self.nepesseg}, főváros: {self.fovaros}, terület: {self.terulet}, népsűrűség: {self.nepsuruseg}"
+    
+
+class Kontinens:
+    def __init__(self, adatok, kontinens):
+        self.lista = []
+        self.eleres = adatok
+        self.kontinens = kontinens
+
+        self.importalas()
+    
+    def orszag_hozzaad(self, orszag):
+        self.lista.append(orszag)
+
+    def importalas(self):
+        file = open(self.eleres, "r", encoding="utf-8")
+        next(file)
+
+        for sor in file:
+            adatok = sor.strip().split(";")
+
+            self.orszag_hozzaad(Orszag(adatok[0], adatok[1], int(adatok[2]), int(adatok[3]), float(adatok[4])))
+
+    def legnepesebb_orszag(self):
+        legnagyobb_nepessegu_orszag = Orszag()
+        for orszag in self.lista:
+            if orszag.nepesseg > legnagyobb_nepessegu_orszag.nepesseg:
+                legnagyobb_nepessegu_orszag = orszag
+
+        return legnagyobb_nepessegu_orszag
+
+    def feladat_kiiras(self):
+        nepesseg_feladat = self.legnepesebb_orszag()
+        print(f"{self.kontinens} legnagyobb népességű ország: {nepesseg_feladat.info()}")
+
+europa = Kontinens("forrasok/europa.txt", "Európa")
+europa.feladat_kiiras()
+
+print()
+
+azsia = Kontinens("forrasok/azsia.txt", "Ázsia")
+azsia.feladat_kiiras()
+
+print()
+
+azsia = Kontinens("forrasok/afrika.txt", "Afrika")
+azsia.feladat_kiiras()
+
+
+
